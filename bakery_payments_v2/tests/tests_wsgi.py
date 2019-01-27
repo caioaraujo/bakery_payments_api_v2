eb18db4de55b4bac995f9072d59559183fa109c0
caioaraujo <c.felipe.araujo@gmail.com>
2019-01-27 12:11:35 -0200

    tests for wsgi module

---
+++ bakery_payments_v2/tests/tests_wsgi.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.core.servers.basehttp import get_internal_wsgi_application
+from django.test import SimpleTestCase, override_settings
+
+from ..wsgi import application
+
+
+class TestWsgi(SimpleTestCase):
+
+    def test__success(self):
+
+        app = get_internal_wsgi_application()
+
+        self.assertIs(app, application)
+
+    @override_settings(WSGI_APPLICATION="wsgi.noexist.app")
+    def test__module_failure(self):
+        expect_message = "WSGI application 'wsgi.noexist.app' could not be loaded; Error importing"
+        with self.assertRaisesMessage(ImproperlyConfigured, expect_message) as context:
+            get_internal_wsgi_application()
