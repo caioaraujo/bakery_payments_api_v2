30b11038e023f33809c33b41e9591c1e6caef71d
caioaraujo <c.felipe.araujo@gmail.com>
2019-02-02 21:50:52 -0200

    filter tests

---
+++ branch/tests/tests_filters.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+
+from ..views import BranchPaymentsView
+from ..filters import PaymentFilterBackend
+
+
+class TestPaymentFilterBackend(SimpleTestCase):
+
+    def test_fields(self):
+        payment_filter = PaymentFilterBackend()
+
+        schema_fields = payment_filter.get_schema_fields(BranchPaymentsView)
+
+        self.assertEqual(1, len(schema_fields))
+
+        is_paid_filter = schema_fields[0]
+        self.assertFalse(is_paid_filter.required)
+        self.assertEqual(is_paid_filter.location, 'query')
